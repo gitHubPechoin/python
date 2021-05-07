359a4733784fdc15a3b467109def95365159e028
zhangy2 <297985420@qq.com>
2021-05-07 16:59:26 +0800

    23

---
+++ if.py
@@ -0,0 +1,29 @@
+# if语句
+
+# a = int(input("请输入年龄："))
+# if 条件后加冒号，注意缩进
+# if a<18:
+#     print("未成年人")
+# else 后加冒号，注意缩进
+# else:
+#     print("成年人")
+
+# a = int(input("请输入年龄："))
+# if a==1:
+#     print(1)
+# else:
+#     print(2)
+
+# a = "我是superman"
+# b = "super"
+# if b in a:
+#     print(1)
+# else:
+#     print(2)
+
+a = [1,2,3,4,5,6,7,8,9]
+b = "2"
+if b in a:
+    print(1)
+else:
+    print(2)
