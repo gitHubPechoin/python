0225c5794d47f942c46da34dd7d9a02a926e4877
zhangy2 <297985420@qq.com>
2021-05-07 11:21:05 +0800

    Git path

---
+++ 1718.py
@@ -0,0 +1,52 @@
+# 元祖tuple,长度固定不可改变,元素为任意内容
+# a = (1,2,3,4,5,6,7,8,9)
+# print(a[0])
+# print(len(a))
+# print(a[-1])
+# print(a[-9])
+# print(a[-10])
+
+# ========================================
+# 列表list,长度可改变,元素为任意内容
+# a = [1,2,3,4,5,6,7,8,9]
+# print(a[0])
+# print(len(a))
+# print(a[-1])
+# print(a[-9])
+
+# 添加元素append()
+# print(a[-1])
+# a.append(a[-1])
+# print(a)
+
+# 清空列表clear()
+# a.clear()
+# print(a)
+
+# 取出并删除pop()
+# a.pop(1)
+# print(a)
+
+# 计数count()
+# b = a.count(1)
+# print(b)
+
+# 获取下表index()
+# print(a.index(1))
+
+# ========================================
+# 字典dict
+# a = {"name":"jay"}
+# # print(a["name"])
+
+# # 取值get()
+# # print(a.get("name"))
+
+# # 修改/新增：如果key存在，则修改；如果key不存在，则新增
+# a["name"] = "Micheal"
+# a.update({"sex":"male"})
+# print(a)
+
+# # 删除
+# a.pop("name")
+# print(a)
